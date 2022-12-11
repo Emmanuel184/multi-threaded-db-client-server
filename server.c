290166ce139f9d69565abe429e304481152221d5
Emmanuel Luis <111292487+Emmanuel184@users.noreply.github.com>
2022-12-10 22:33:40 -0600

    added get/put methods

--- server.c
+++ server.c
@@ -9,25 +9,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+typedef struct studentRecord
+{
+  char name[30]; // student name
+  int32_t sid;   // id of the student
+  int32_t index; // index of this record in the data base file
+} sr;
+
 void Usage(char *progname);
 void PrintOut(int fd, struct sockaddr *addr, size_t addrlen);
 void PrintReverseDNS(struct sockaddr *addr, size_t addrlen);
 void PrintServerSide(int client_fd, int sock_family);
-int  Listen(char *portnum, int *sock_family);
+int Listen(char *portnum, int *sock_family);
 void HandleClient(int c_fd, struct sockaddr *addr, size_t addrlen,
                   int sock_family);
+void put(int32_t fd);
+void get(int32_t fd);
 
-int 
-main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
   // Expect the port number as a command line argument.
-  if (argc != 2) {
+  if (argc != 2)
+  {
     Usage(argv[0]);
   }
 
   int sock_family;
   int listen_fd = Listen(argv[1], &sock_family);
-  if (listen_fd <= 0) {
+  if (listen_fd <= 0)
+  {
     // We failed to bind/listen to a socket.  Quit with failure.
     printf("Couldn't bind to any addresses.\n");
     return EXIT_FAILURE;
@@ -35,13 +45,15 @@ main(int argc, char **argv)
 
   // Loop forever, accepting a connection from a client and doing
   // an echo trick to it.
-  while (1) {
+  while (1)
+  {
     struct sockaddr_storage caddr;
     socklen_t caddr_len = sizeof(caddr);
     int client_fd = accept(listen_fd,
                            (struct sockaddr *)(&caddr),
                            &caddr_len);
-    if (client_fd < 0) {
+    if (client_fd < 0)
+    {
       if ((errno == EINTR) || (errno == EAGAIN) || (errno == EWOULDBLOCK))
         continue;
       printf("Failure on accept:%d \n ", strerror(errno));
@@ -59,24 +71,82 @@ main(int argc, char **argv)
   return EXIT_SUCCESS;
 }
 
-void Usage(char *progname) {
+void 
+
+    
+    put(int32_t fd)
+{
+  sr s;
+
+  printf("Enter student name: ");
+
+  fgets(s.name, 30, stdin);
+
+  s.name[strlen(s.name) - 1] = '\0';
+
+  printf("Enter the student id: ");
+
+  scanf("%d", &s.sid);
+
+  printf("Enter the record index: ");
+
+  scanf("%d", &s.index);
+
+  lseek(fd, size(sr) * s.index, SEEK_SET);
+
+  write(fd, &s, sizeof(sr));
+}
+
+void get(int32_t fd)
+{
+  sr s;
+
+  int32_t index;
+
+  printf("Enter the record index");
+
+  scanf("%d", s.index);
+
+  if ((lseek(fd, sizeof(sr) * index, 0) >= lseek(fd, 0, SEEK_END)) || index < 0)
+  {
+    fprintf(stderr, "The given index is out of bounds\n");
+    return;
+  }
+
+  lseek(fd, sizeof(sr) * index, SEEK_SET);
+
+  read(fd, &s, sizeof(sr));
+
+  if (s.name[0] == NULL)
+  {
+    fprintf(stderr, "No student record exists at the given index");
+
+    return;
+  }
+
+  printf("Student name %s \n", s.name);
+  printf("Student id %d \n", s.sid);
+  printf("Student index: %d \n", s.index);
+}
+
+void Usage(char *progname)
+{
   printf("usage: %s port \n", progname);
   exit(EXIT_FAILURE);
 }
 
-
-int 
-Listen(char *portnum, int *sock_family) {
+int Listen(char *portnum, int *sock_family)
+{
 
   // Populate the "hints" addrinfo structure for getaddrinfo().
   // ("man addrinfo")
   struct addrinfo hints;
   memset(&hints, 0, sizeof(struct addrinfo));
   hints.ai_family = AF_INET;       // IPv6 (also handles IPv4 clients)
-  hints.ai_socktype = SOCK_STREAM;  // stream
-  hints.ai_flags = AI_PASSIVE;      // use wildcard "in6addr_any" address
-  hints.ai_flags |= AI_V4MAPPED;    // use v4-mapped v6 if no v6 found
-  hints.ai_protocol = IPPROTO_TCP;  // tcp protocol
+  hints.ai_socktype = SOCK_STREAM; // stream
+  hints.ai_flags = AI_PASSIVE;     // use wildcard "in6addr_any" address
+  hints.ai_flags |= AI_V4MAPPED;   // use v4-mapped v6 if no v6 found
+  hints.ai_protocol = IPPROTO_TCP; // tcp protocol
   hints.ai_canonname = NULL;
   hints.ai_addr = NULL;
   hints.ai_next = NULL;
@@ -87,8 +157,9 @@ Listen(char *portnum, int *sock_family) {
   int res = getaddrinfo(NULL, portnum, &hints, &result);
 
   // Did addrinfo() fail?
-  if (res != 0) {
-	printf( "getaddrinfo failed: %s", gai_strerror(res));
+  if (res != 0)
+  {
+    printf("getaddrinfo failed: %s", gai_strerror(res));
     return -1;
   }
 
@@ -97,11 +168,13 @@ Listen(char *portnum, int *sock_family) {
   // linked in a list through the "ai_next" field of result.
   int listen_fd = -1;
   struct addrinfo *rp;
-  for (rp = result; rp != NULL; rp = rp->ai_next) {
+  for (rp = result; rp != NULL; rp = rp->ai_next)
+  {
     listen_fd = socket(rp->ai_family,
                        rp->ai_socktype,
                        rp->ai_protocol);
-    if (listen_fd == -1) {
+    if (listen_fd == -1)
+    {
       // Creating this socket failed.  So, loop to the next returned
       // result and try again.
       printf("socket() failed:%d \n ", strerror(errno));
@@ -119,10 +192,11 @@ Listen(char *portnum, int *sock_family) {
 
     // Try binding the socket to the address and port number returned
     // by getaddrinfo().
-    if (bind(listen_fd, rp->ai_addr, rp->ai_addrlen) == 0) {
+    if (bind(listen_fd, rp->ai_addr, rp->ai_addrlen) == 0)
+    {
       // Bind worked!  Print out the information about what
       // we bound to.
-      //PrintOut(listen_fd, rp->ai_addr, rp->ai_addrlen);
+      // PrintOut(listen_fd, rp->ai_addr, rp->ai_addrlen);
 
       // Return to the caller the address family.
       *sock_family = rp->ai_family;
@@ -143,7 +217,8 @@ Listen(char *portnum, int *sock_family) {
     return listen_fd;
 
   // Success. Tell the OS that we want this to be a listening socket.
-  if (listen(listen_fd, SOMAXCONN) != 0) {
+  if (listen(listen_fd, SOMAXCONN) != 0)
+  {
     printf("Failed to mark socket as listening:%d \n ", strerror(errno));
     close(listen_fd);
     return -1;
@@ -153,31 +228,33 @@ Listen(char *portnum, int *sock_family) {
   return listen_fd;
 }
 
-void 
-HandleClient(int c_fd, struct sockaddr *addr, size_t addrlen,
-                  int sock_family) 
+void HandleClient(int c_fd, struct sockaddr *addr, size_t addrlen,
+                  int sock_family)
 {
   // Print out information about the client.
-  printf("\nNew client connection \n" );
+  printf("\nNew client connection \n");
   PrintOut(c_fd, addr, addrlen);
   PrintReverseDNS(addr, addrlen);
   PrintServerSide(c_fd, sock_family);
 
   // Loop, reading data and echo'ing it back, until the client
   // closes the connection.
-  while (1) {
+  while (1)
+  {
     char clientbuf[1024];
     ssize_t res = read(c_fd, clientbuf, 1023);
-    if (res == 0) {
+    if (res == 0)
+    {
       printf("[The client disconnected.] \n");
       break;
     }
 
-    if (res == -1) {
+    if (res == -1)
+    {
       if ((errno == EAGAIN) || (errno == EINTR))
         continue;
 
-	  printf(" Error on client socket:%d \n ", strerror(errno));
+      printf(" Error on client socket:%d \n ", strerror(errno));
       break;
     }
     clientbuf[res] = '\0';
@@ -191,11 +268,11 @@ HandleClient(int c_fd, struct sockaddr *addr, size_t addrlen,
 
   close(c_fd);
 }
-void 
-PrintOut(int fd, struct sockaddr *addr, size_t addrlen) 
+void PrintOut(int fd, struct sockaddr *addr, size_t addrlen)
 {
   printf("Socket [%d] is bound to: \n", fd);
-  if (addr->sa_family == AF_INET) {
+  if (addr->sa_family == AF_INET)
+  {
     // Print out the IPV4 address and port
 
     char astring[INET_ADDRSTRLEN];
@@ -203,8 +280,9 @@ PrintOut(int fd, struct sockaddr *addr, size_t addrlen)
     inet_ntop(AF_INET, &(in4->sin_addr), astring, INET_ADDRSTRLEN);
     printf(" IPv4 address %s", astring);
     printf(" and port %d\n", ntohs(in4->sin_port));
-
-  } else if (addr->sa_family == AF_INET6) {
+  }
+  else if (addr->sa_family == AF_INET6)
+  {
     // Print out the IPV6 address and port
 
     char astring[INET6_ADDRSTRLEN];
@@ -212,51 +290,56 @@ PrintOut(int fd, struct sockaddr *addr, size_t addrlen)
     inet_ntop(AF_INET6, &(in6->sin6_addr), astring, INET6_ADDRSTRLEN);
     printf("IPv6 address %s", astring);
     printf(" and port %d\n", ntohs(in6->sin6_port));
-
-  } else {
+  }
+  else
+  {
     printf(" ???? address and port ???? \n");
   }
 }
 
-void 
-PrintReverseDNS(struct sockaddr *addr, size_t addrlen) {
-  char hostname[1024];  // ought to be big enough.
-  if (getnameinfo(addr, addrlen, hostname, 1024, NULL, 0, 0) != 0) {
+void PrintReverseDNS(struct sockaddr *addr, size_t addrlen)
+{
+  char hostname[1024]; // ought to be big enough.
+  if (getnameinfo(addr, addrlen, hostname, 1024, NULL, 0, 0) != 0)
+  {
     sprintf(hostname, "[reverse DNS failed]");
   }
   printf("DNS name: %s \n", hostname);
 }
 
-void 
-PrintServerSide(int client_fd, int sock_family) {
+void PrintServerSide(int client_fd, int sock_family)
+{
   char hname[1024];
   hname[0] = '\0';
 
   printf("Server side interface is ");
-  if (sock_family == AF_INET) {
+  if (sock_family == AF_INET)
+  {
     // The server is using an IPv4 address.
     struct sockaddr_in srvr;
     socklen_t srvrlen = sizeof(srvr);
     char addrbuf[INET_ADDRSTRLEN];
-    getsockname(client_fd, (struct sockaddr *) &srvr, &srvrlen);
+    getsockname(client_fd, (struct sockaddr *)&srvr, &srvrlen);
     inet_ntop(AF_INET, &srvr.sin_addr, addrbuf, INET_ADDRSTRLEN);
     printf("%s", addrbuf);
     // Get the server's dns name, or return it's IP address as
     // a substitute if the dns lookup fails.
-    getnameinfo((const struct sockaddr *) &srvr,
+    getnameinfo((const struct sockaddr *)&srvr,
                 srvrlen, hname, 1024, NULL, 0, 0);
     printf(" [%s]\n", hname);
-  } else {
+  }
+  else
+  {
     // The server is using an IPv6 address.
     struct sockaddr_in6 srvr;
     socklen_t srvrlen = sizeof(srvr);
     char addrbuf[INET6_ADDRSTRLEN];
-    getsockname(client_fd, (struct sockaddr *) &srvr, &srvrlen);
+    getsockname(client_fd, (struct sockaddr *)&srvr, &srvrlen);
     inet_ntop(AF_INET6, &srvr.sin6_addr, addrbuf, INET6_ADDRSTRLEN);
     printf("%s", addrbuf);
     // Get the server's dns name, or return it's IP address as
     // a substitute if the dns lookup fails.
-    getnameinfo((const struct sockaddr *) &srvr,
+    getnameinfo((const struct sockaddr *)&srvr,
                 srvrlen, hname, 1024, NULL, 0, 0);
     printf(" [%s]\n", hname);
   }
